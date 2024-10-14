1cd9754976e387ee08f45640b487c29194015328
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-10-14 12:43:46 +0300

    Products Router setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 
+import productsRoutes from "./routes/products";
+
 const app = express();
+
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/products", productsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
